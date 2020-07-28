6d48357a80d4ac80c46dd740ed5f983f0a3c60fe
David Omar Flores Chávez <david@davidomar.com>
2020-07-27 20:04:59 -0500

    Create the JobListing module and component using `ng g`

---
+++ src/app/job-listing/job-listing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { JobListingComponent } from "./job-listing.component";
+
+@NgModule({
+  declarations: [JobListingComponent],
+  imports: [CommonModule],
+  exports: [JobListingComponent]
+})
+export class JobListingModule {}
