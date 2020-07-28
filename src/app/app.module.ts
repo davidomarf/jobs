3ce77314416b47b90dc119470bc20b0f47e33bae
David Omar Flores Chávez <david@davidomar.com>
2020-07-27 20:06:19 -0500

    Import and render mock content from the JobListing and Header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { JobListingModule } from "./job-listing/job-listing.module";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, JobListingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
