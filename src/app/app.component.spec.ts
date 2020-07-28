99e07dcf1ea5b83e0f51c82453bf97202e28e395
David Omar Flores Chávez <david@davidomar.com>
2020-07-27 19:36:43 -0500

    Remove the auto-generated content from `ng new`
    Keep the files but remove the content

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,4 @@ describe("AppComponent", () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'job-listings'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual("job-listings");
-  });
-
-  it("should render title", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector(".content span").textContent).toContain(
-      "job-listings app is running!"
-    );
-  });
 });
