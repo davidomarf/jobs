80c3ee8652078df3b40e6fb0696d71fa7b62c4da
David Omar Flores Chávez <david@davidomar.com>
2020-08-16 00:17:47 -0500

    Use template instead of templateUrl in tagsComponetn

--- src/app/job-listing/offer/tags/tags.component.ts
+++ src/app/job-listing/offer/tags/tags.component.ts
@@ -2,11 +2,17 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tags',
-  templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.scss'],
+  template: `
+    <div class="tags">
+      <div *ngFor="let tag of tags" class="tag">
+        {{ tag }}
+      </div>
+    </div>
+  `,
 })
 export class TagsComponent {
   @Input() public readonly tags: string[] = [];
-  
+
   constructor() {}
 }
