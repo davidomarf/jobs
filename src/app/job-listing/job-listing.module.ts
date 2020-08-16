85b41bfc052e002d890ec8b72e97fac6042858b4
David Omar Flores Chávez <david@davidomar.com>
2020-08-15 22:07:03 -0500

    Take out the tags layout into a new separate component

--- src/app/job-listing/job-listing.module.ts
+++ src/app/job-listing/job-listing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JobListingComponent } from './job-listing.component';
 import { OfferComponent } from './offer/offer.component';
+import { TagsComponent } from './offer/tags/tags.component';
 
 @NgModule({
-  declarations: [JobListingComponent, OfferComponent],
+  declarations: [JobListingComponent, OfferComponent, TagsComponent],
   imports: [CommonModule],
   exports: [JobListingComponent],
 })
