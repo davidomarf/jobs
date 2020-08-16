e726428e9336c245297422b282a729bb69395e56
David Omar Flores Chávez <david@davidomar.com>
2020-08-16 02:00:55 -0500

    Use the real path for the offers JSON file using @environment

--- src/app/job-listing/job-listing.service.ts
+++ src/app/job-listing/job-listing.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { JobOffer } from './offer/offer.component';
 import { Observable } from 'rxjs';
+import { environment } from '@environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JobListingService {
-  private readonly path = './';
+  private readonly path = environment.OFFERS_URL;
 
   constructor(private readonly http: HttpClient) {}
 
