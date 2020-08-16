a00cd2f4f4f13b7690944887bcb3fbad343569b4
David Omar Flores Chávez <david@davidomar.com>
2020-08-16 00:49:20 -0500

    Use the flag component and remove info.html due to its short size

--- src/app/job-listing/offer/info/info.component.ts
+++ src/app/job-listing/offer/info/info.component.ts
@@ -2,8 +2,21 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info',
-  templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss'],
+  template: `
+    <div class="main">
+      <div class="header">
+        <p class="company">
+          {{ company }}
+        </p>
+        <app-flags [flags]="flags"></app-flags>
+      </div>
+      <p class="position">{{ position }}</p>
+      <div class="meta">
+        <span *ngFor="let item of meta">{{ item }}</span>
+      </div>
+    </div>
+  `,
 })
 export class InfoComponent implements OnInit {
   @Input() public readonly company = '';
