a1ab8b0414296ceeb2bf2006d4263a1453c550c7
David Omar Flores Chávez <david@davidomar.com>
2020-08-15 22:29:58 -0500

    Add Input to the OfferComponent

--- src/app/job-listing/offer/offer.component.ts
+++ src/app/job-listing/offer/offer.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
-  styleUrls: ['./offer.component.scss']
+  styleUrls: ['./offer.component.scss'],
 })
 export class OfferComponent implements OnInit {
+  @Input() offer: JobOffer = {} as JobOffer;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
+}
 
+type JobOffer = {
+  id: number;
+  company: string;
+  logo: string;
+
+  new: boolean;
+  featured: boolean;
+
+  position: string;
+  role: string;
+  level: string;
+
+  postedAt: string;
+  contract: string;
+  location: string;
+
+  languages: string[];
+  tools: string[];
 }
